7dd35961af9c814263633a8c62f461c089524c2b
mamconake <78598838+mamconake@users.noreply.github.com>
2022-04-20 12:02:54 +0300

    Updating parser.c

--- parser.c
+++ parser.c
@@ -1,25 +1,26 @@
 #include "main.h"
+
 /**
- ** parser - Receives all the main string and
- **all the necessary parameters to print a formatted string
- ** @format: String containing all desired characters
- ** @f_list: List of all possible functions
- ** @arg_list: List containing all arguments passed to the program
- ** Return: Printed characters.
- **/
-int parser(const char *format, cover_t f_list[], va_list arg_list)
+ * parser - Receives the main string and all the necessary parameters to
+ * print a formated string.
+ * @format: A string containing all the desired characters.
+ * @f_list: A list of all the posible functions.
+ * @arg_list: A list containing all the argumentents passed to the program.
+ * Return: A total count of the characters printed.
+ */
+int parser(const char *format, conver_t f_list[], va_list arg_list)
 {
 	int i, j, r_val, printed_chars;
 
 	printed_chars = 0;
-
-	for (i = 0; format[i] != '\0', i++)
+	for (i = 0; format[i] != '\0'; i++)/* Iterates through the main str*/
 	{
-		if (format[i] == '%')
+		if (format[i] == '%') /*Checks for format specifiers*/
 		{
+			/*Iterates through struct to find the right func*/
 			for (j = 0; f_list[j].sym != NULL; j++)
 			{
-				if (format[i + 1] ++f_list[j].sym[0])
+				if (format[i + 1] == f_list[j].sym[0])
 				{
 					r_val = f_list[j].f(arg_list);
 					if (r_val == -1)
@@ -39,11 +40,11 @@ int parser(const char *format, cover_t f_list[], va_list arg_list)
 				else
 					return (-1);
 			}
-			i = i + 1;
+			i = i + 1; /*Updating i to skip format symbols*/
 		}
 		else
 		{
-			_write_char(format[i]);
+			_write_char(format[i]); /*call the write function*/
 			printed_chars++;
 		}
 	}
